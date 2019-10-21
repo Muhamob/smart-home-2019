09d087a98433f20928e66c43342d8948a051a5f7
muhamob <yusov.aa@phystech.edu>
2019-10-21 12:38:14 +0300

    Добавил обработчики событий активации/деактивациии сигнализации

--- src/main/java/ru/sbt/mipt/oop/SensorEventType.java
+++ src/main/java/ru/sbt/mipt/oop/SensorEventType.java
@@ -1,5 +1,15 @@
 package ru.sbt.mipt.oop;
 
 public enum SensorEventType {
-    LIGHT_ON, LIGHT_OFF, DOOR_OPEN, DOOR_CLOSED
+    LIGHT_ON, LIGHT_OFF, DOOR_OPEN, DOOR_CLOSED, ALARM_ACTIVATE, ALARM_DEACTIVATE;
+
+    private String code;
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
