491efc4a48cf554adf1496e38c3b96cb1c6abc55
muhamob <yusov.aa@phystech.edu>
2019-12-01 15:27:06 +0300

    Fix: SRP in EventList. Now EventCollectionCreator is responsible for handling alarm states in actions

--- src/main/java/ru/sbt/mipt/oop/eventHandlers/EventCollectionCreator.java
+++ src/main/java/ru/sbt/mipt/oop/eventHandlers/EventCollectionCreator.java
@@ -1,16 +1,45 @@
 package ru.sbt.mipt.oop.eventHandlers;
 
 import ru.sbt.mipt.oop.SensorEvent;
-import ru.sbt.mipt.oop.actions.AlarmAction;
-import ru.sbt.mipt.oop.actions.DoorScenario;
-import ru.sbt.mipt.oop.actions.HomeComponentAction;
-import ru.sbt.mipt.oop.actions.SwitchLightById;
+import ru.sbt.mipt.oop.actions.*;
+import ru.sbt.mipt.oop.devices.Alarm;
+import ru.sbt.mipt.oop.homeStructure.SmartHome;
+import ru.sbt.mipt.oop.homeUtils.AlarmUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class EventCollectionCreator {
-    public static List<HomeComponentAction> getActionList(SensorEvent event) {
+    private final Alarm alarm;
+    private final SmartHome home;
+
+    public EventCollectionCreator(SmartHome home) {
+        this.home = home;
+        this.alarm = AlarmUtils.getAlarm(home);
+    }
+
+    public List<HomeComponentAction> getActionList(SensorEvent event) {
+        List<HomeComponentAction> actions = new ArrayList<>();
+
+        for (HomeComponentAction action : getDefaultActionList(event)) {
+            if (alarm != null) {
+                if (alarm.isActivated()) {
+                    if (action.getClass() != AlarmAction.class) {
+                        action = new ActionWhileAlarmActivatedDecorator(action);
+                    }
+                } else if (alarm.isAlerting()) {
+                    action = new ActionWhileAlarmAlertDecorator(action);
+                }
+            }
+
+            actions.add(action);
+        }
+
+        return actions;
+    }
+
+    private List<HomeComponentAction> getDefaultActionList(SensorEvent event) {
         return Arrays.asList(
                 new DoorScenario(event),
                 new SwitchLightById(event),
--- src/main/java/ru/sbt/mipt/oop/eventHandlers/EventList.java
+++ src/main/java/ru/sbt/mipt/oop/eventHandlers/EventList.java
@@ -1,40 +1,19 @@
 package ru.sbt.mipt.oop.eventHandlers;
 
 import ru.sbt.mipt.oop.SensorEvent;
-import ru.sbt.mipt.oop.actions.ActionWhileAlarmActivatedDecorator;
-import ru.sbt.mipt.oop.actions.ActionWhileAlarmAlertDecorator;
-import ru.sbt.mipt.oop.actions.AlarmAction;
 import ru.sbt.mipt.oop.actions.HomeComponentAction;
-import ru.sbt.mipt.oop.devices.Alarm;
 import ru.sbt.mipt.oop.homeStructure.SmartHome;
-import ru.sbt.mipt.oop.homeUtils.AlarmUtils;
 
 import java.util.List;
 
 public class EventList {
     public static void run(SmartHome smartHome, SensorEvent event) {
-        List<HomeComponentAction> actions = EventCollectionCreator.getActionList(event);
+        EventCollectionCreator eventCollectionCreator = new EventCollectionCreator(smartHome);
+        List<HomeComponentAction> actions = eventCollectionCreator.getActionList(event);
 
         for (HomeComponentAction action : actions) {
-            // Декоратор для событий, если сигнализация забила тревогу
-
-            // Трюк, чтобы можно было внутри лямбда выражения назначить
-            // переменную из внешней области видимости
-            Alarm alarm = AlarmUtils.getAlarm(smartHome);
-
-            if (alarm != null) {
-
-                if (alarm.isActivated()) {
-                    if (action.getClass() != AlarmAction.class) {
-                        action = new ActionWhileAlarmActivatedDecorator(action);
-                    }
-                } else if (alarm.isAlerting()) {
-                    action = new ActionWhileAlarmAlertDecorator(action);
-                }
-
-            }
+            // Used in debug mode, don't know how to create proper logger in java
             System.out.println("Action is: " + action);
-
             smartHome.execute(action);
         }
     }
